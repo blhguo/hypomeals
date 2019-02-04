83bf5e57d4e9702d497ad06bddd37358cc955969
Morton Mo <ym84@duke.edu>
2019-02-04 12:29:02 -0500

    Fixed lint / tweaked linter settings

--- hypomeals/HypoMeals/settings.py
+++ hypomeals/HypoMeals/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "v*hru*y3-fk)j!=*qi50y_da^1v^2&32d0^-91o)67u*57hse-"
+SECRET_KEY = "v*hru*y3-fk)j!=*qi50y_da^1v^2&32d0^-91o)67u*57hse-"  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- hypomeals/meals/views/__init__.py
+++ hypomeals/meals/views/__init__.py
@@ -1,3 +1,5 @@
+# flake8: noqa
+
 from .admin import *
 from .autocomplete import *
 from .bulk import *
