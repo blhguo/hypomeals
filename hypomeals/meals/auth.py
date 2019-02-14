d7584a376fa56cefcb8fbda9475e0c7c8e651de8
Yehan Mo (ym84) <ym84@duke.edu>
2019-02-14 00:59:39 -0500

    NetID Authentication

--- hypomeals/meals/auth.py
+++ hypomeals/meals/auth.py
@@ -3,10 +3,13 @@ A series of functions / decorators to aid authentication and permission checking
 """
 import functools
 
+from django.contrib.auth import login
+from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 
 from meals import utils
+from meals.models import User
 
 
 @utils.parameterized
@@ -32,3 +35,31 @@ def permission_required_ajax(func, perm):
         )
 
     return wrapper
+
+
+def sign_in_netid_user(request, identity):
+    """
+    Process an identity response from Duke identity API: https://apidocs.colab.duke.edu/
+
+    If a NetID user already exists, log the user in. Otherwise, create a new user with
+    a NetID association, add to the Users group, and log the user in.
+    :param request: the HTTP request from the browser after landing
+    :param identity: the identity blob returned by the identity API
+    :return: True if the user is logged in. False otherwise.
+    """
+    if "netid" not in identity:
+        return False
+    user, created = User.objects.get_or_create(
+        netid=identity["netid"],
+        defaults={
+            "first_name": identity.get("nickname", identity.get("firstName", "")),
+            "last_name": identity.get("lastName", ""),
+            "email": identity.get("mail", ""),
+            "username": f"netid_user_{identity['netid']}",
+        },
+    )
+    if created:
+        users_group = Group.objects.get(name="Users")
+        user.groups.add(users_group)
+    login(request, user)
+    return True
