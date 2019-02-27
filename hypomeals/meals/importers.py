4ed7cba983ab592c4435fd75c0a5ac3c5f2299f4
Morton Mo <ym84@duke.edu>
2019-02-27 07:30:11 -0500

    Formula seems to work

--- hypomeals/meals/importers.py
+++ hypomeals/meals/importers.py
@@ -1,11 +1,14 @@
-#pylint: disable-msg=protected-access,unused-argument,not-callable
+# pylint: disable-msg=protected-access,unused-argument,not-callable
 import copy
+import csv
 import logging
 import re
 from abc import ABC
 from collections import defaultdict
+from decimal import Decimal, InvalidOperation
 from typing import List, Dict, Any, Tuple
 
+from django.core.exceptions import ValidationError
 from django.db.models import Model, AutoField, Field
 
 from meals import utils
@@ -14,9 +17,20 @@ from meals.exceptions import (
     AmbiguousRecordException,
     CollisionException,
     DuplicateException,
+    UserFacingException,
+    Skip,
 )
-from meals.models import Sku, ProductLine, Upc, Vendor, Ingredient, FormulaIngredient, \
-    ManufacturingLine, Formula
+from meals.models import (
+    Sku,
+    ProductLine,
+    Upc,
+    Vendor,
+    Ingredient,
+    FormulaIngredient,
+    ManufacturingLine,
+    Formula,
+    Unit,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -49,10 +63,17 @@ class Importer(ABC):
                 if field.unique
             ] + list(self.model._meta.unique_together)
 
-    def do_import(self, reader, filename=None):
+    def do_import(self, lines, filename=None):
+        reader = csv.DictReader(lines[1:], fieldnames=self.header, dialect="unix")
         num_processed = 0
         for line_num, row in enumerate(reader, start=2):
-            converted_row = self._process_row(copy.copy(row), line_num)
+            try:
+                converted_row = self._process_row(copy.copy(row), filename, line_num)
+            except UserFacingException as e:
+                e.message = f"{filename}:{line_num}: " + e.message
+                raise e
+            except Skip:
+                continue
             instance = self._construct_instance(converted_row)
             self._check_duplicates(row, converted_row, filename, line_num)
             try:
@@ -77,8 +98,7 @@ class Importer(ABC):
     def _check_duplicates(self, raw, converted, filename, line_num):
         for unique_keys in self.unique_fields:
             values = tuple(
-                converted[self.field_dict[unique_key]]
-                for unique_key in unique_keys
+                converted[self.field_dict[unique_key]] for unique_key in unique_keys
             )
             if values in self.unique_dict[unique_keys]:
                 key_name = tuple(
@@ -89,7 +109,6 @@ class Importer(ABC):
                 )
                 previous_line_num = self.unique_dict[unique_keys][values]
                 raise DuplicateException(
-                    f"{filename}:{line_num}: "
                     f"Cannot import {self.file_type} with {key_name} = "
                     f"'{raw_values}'.",
                     model_name=self.model_name,
@@ -100,7 +119,7 @@ class Importer(ABC):
 
             self.unique_dict[unique_keys][values] = line_num
 
-    def _process_row(self, row, line_num=None):
+    def _process_row(self, row, filename=None, line_num=None):
         return row
 
     def _construct_instance(self, row, line_num=None):
@@ -135,7 +154,7 @@ class Importer(ABC):
                 break
         if len(matches) > 1:
             raise AmbiguousRecordException(
-                f"{filename}:{line_num}: Ambiguous record detected.", instance, matches
+                f"Ambiguous record detected.", instance, matches
             )
         if primary_key_value:
             primary_match = model.objects.filter(
@@ -147,17 +166,14 @@ class Importer(ABC):
         if len(matches) == 1:
             if not actual_match:
                 raise AmbiguousRecordException(
-                    f"{filename}:{line_num}: Primary key did not match record while "
-                    "other keys did.",
+                    f"Primary key did not match record while " "other keys did.",
                     instance,
                     matches,
                 )
             if next(iter(matches.values())) != actual_match:
                 matches[self.primary_key.verbose_name] = actual_match
                 raise AmbiguousRecordException(
-                    f"{filename}:{line_num}: Ambiguous record detected.",
-                    instance,
-                    matches,
+                    f"Ambiguous record detected.", instance, matches
                 )
         if actual_match:
             if model.compare_instances(actual_match, instance):
@@ -181,7 +197,7 @@ class SkuImporter(Importer):
         "Unit UPC",
         "Unit size",
         "Count per case",
-        "Product Line Name",
+        "PL Name",
         "Formula#",
         "Formula factor",
         "ML Shortnames",
@@ -203,10 +219,11 @@ class SkuImporter(Importer):
         "formula": "Formula#",
         "formula_scale": "Formula factor",
         "manufacturing_lines": "ML Shortnames",
+        "manufacturing_rate": "Rate",
         "comment": "Comment",
     }
 
-    def _process_row(self, row, line_num=None):
+    def _process_row(self, row, filename=None, line_num=None):
         raw_formula = row["Formula#"]
 
         formula_qs = Formula.objects.filter(number=raw_formula)
@@ -217,36 +234,41 @@ class SkuImporter(Importer):
                 message=f"Cannot import SKU #{row['SKU#']}",
                 line_num=line_num,
                 referring_name="SKU",
-                referred_name="Product Line",
+                referred_name="Formula",
                 fk_name="Formula",
                 fk_value=row["Formula#"],
             )
         raw_case_upc = row["Case UPC"]
-        print(utils.is_valid_upc(raw_case_upc))
         if utils.is_valid_upc(raw_case_upc):
-            print("made it")
             row["Case UPC"] = Upc.objects.get_or_create(upc_number=raw_case_upc)[0]
-            print(row['Case UPC'])
+        else:
+            raise UserFacingException(f"{raw_case_upc} is not a valid UPC.")
         raw_unit_upc = row["Unit UPC"]
         if utils.is_valid_upc(raw_unit_upc):
             row["Unit UPC"] = Upc.objects.get_or_create(upc_number=raw_unit_upc)[0]
+        else:
+            raise UserFacingException(f"{raw_unit_upc} is not a valid UPC.")
         if row["Comment"] is None:
             row["Comment"] = ""
-        product_line = ProductLine.objects.filter(name=row["Product Line Name"])
+        product_line = ProductLine.objects.filter(name=row["PL Name"])
         if product_line.exists():
-            row["Product Line Name"] = product_line[0]
+            row["PL Name"] = product_line[0]
         else:
             raise IntegrityException(
                 message=f"Cannot import SKU #{row['SKU#']}",
                 line_num=line_num,
                 referring_name="SKU",
                 referred_name="Product Line",
-                fk_name="Product Line Name",
-                fk_value=row["Product Line Name"],
+                fk_name="PL Name",
+                fk_value=row["PL Name"],
             )
 
         # Duplicated shortnames are ignored
-        raw_ml = set(re.split(r",\s*", row["ML Shortnames"]))
+        raw_ml = {
+            shortname.strip()
+            for shortname in re.split(r",\s*", row["ML Shortnames"])
+            if shortname.strip()
+        }
         ml_objs = ManufacturingLine.objects.filter(shortname__in=raw_ml)
         found = set(ml_objs.values_list("shortname", flat=True))
         missing = raw_ml - found
@@ -278,10 +300,23 @@ class IngredientImporter(Importer):
         "vendor": "Vendor Info",
         "size": "Size",
         "cost": "Cost",
+        "unit": "Unit",  # this column doesn't actually exist
         "comment": "Comment",
     }
 
-    def _process_row(self, row, line_num=None):
+    def _process_row(self, row, filename=None, line_num=None):
+        raw_size = row["Size"]
+        try:
+            row["Size"], row["Unit"] = Unit.from_exp(raw_size)
+        except RuntimeError as e:
+            raise UserFacingException(str(e))
+        try:
+            row["Size"] = Decimal(row["Size"])
+        except InvalidOperation:
+            raise UserFacingException(
+                f"{row['Size']}is not a valid floating point number"
+            )
+
         vendor = Vendor.objects.filter(info=row["Vendor Info"])
         if vendor.exists():
             row["Vendor Info"] = vendor[0]
@@ -292,30 +327,35 @@ class IngredientImporter(Importer):
         return row
 
 
-class FormulaImporter(Importer):
+class FormulaIngredientImporter(Importer):
 
     file_type = "formulas"
-    header = ["SKU#", "Ingr#", "Quantity"]
+    header = ["Formula#", "Name", "Ingr#", "Quantity", "Comment"]
     field_dict = {
-        "sku_number": "SKU#",
-        "ingredient_number": "Ingr#",
+        "formula": "Formula#",
+        "ingredient": "Ingr#",
         "quantity": "Quantity",
+        "unit": "Unit",
     }
     model = FormulaIngredient
     model_name = "Formula"
+    primary_key = (
+        FormulaIngredient._meta.get_field("formula"),
+        FormulaIngredient._meta.get_field("ingredient"),
+    )
 
-    def _process_row(self, row, line_num=None):
-        sku = Sku.objects.filter(number=row["SKU#"])
-        if not sku.exists():
-            raise IntegrityException(
-                message="Cannot import Formula",
-                line_num=line_num,
-                referring_name="Formula",
-                referred_name="SKU",
-                fk_name="SKU#",
-                fk_value=row["SKU#"],
-            )
-        row["SKU#"] = sku[0]
+    def __init__(self, formulas: Dict[str, Formula]):
+        super().__init__()
+        self.formulas = formulas
+
+    def _process_row(self, row, filename=None, line_num=None):
+        raw_size = row["Quantity"]
+        try:
+            row["Quantity"], row["Unit"] = Unit.from_exp(raw_size)
+        except RuntimeError as e:
+            raise UserFacingException(str(e))
+
+        row["Formula#"] = self.formulas[row["Name"]]
         ingr = Ingredient.objects.filter(number=row["Ingr#"])
         if not ingr.exists():
             raise IntegrityException(
@@ -334,10 +374,57 @@ class FormulaImporter(Importer):
         return instance
 
     def _post_process(self):
-        sku_numbers = {formula.sku_number.number for formula in self.instances}
-        FormulaIngredient.objects.filter(sku_number__in=sku_numbers).delete()
-        for instance in self.instances:
-            instance.save()
+        formulas = {formula.pk for formula in self.formulas.values()}
+        FormulaIngredient.objects.filter(pk__in=formulas).delete()
+        FormulaIngredient.objects.bulk_create(self.instances)
+
+
+class FormulaImporter(Importer):
+    file_type = "formulas"
+    header = ["Formula#", "Name", "Ingr#", "Quantity", "Comment"]
+    field_dict = {"name": "Name", "number": "Formula#", "comment": "Comment"}
+    model = Formula
+    model_name = "Formula"
+    primary_key = Formula._meta.get_field("number")
+
+    def __init__(self):
+        super().__init__()
+
+    def do_import(self, lines, filename=None):
+        lines_copy = copy.copy(lines)
+        reader = csv.DictReader(lines[1:], fieldnames=self.header, dialect="unix")
+        num_processed = 0
+        for line_num, row in enumerate(reader, start=2):
+            instance = self._construct_instance(row)
+            try:
+                self._check_duplicates(row, row, filename, line_num)
+            except DuplicateException:
+                continue
+            try:
+                instance = self._save(instance, filename, line_num)
+            except CollisionException as e:
+                self.collisions.append(e)
+            else:
+                self.instances.append(instance)
+
+            num_processed += 1
+
+        logger.info(
+            "Processed %d records of model %s (%d collisions)",
+            num_processed,
+            self.model_name,
+            len(self.collisions),
+        )
+        if not self.collisions:
+            self._post_process()
+
+        ingr, ingr_collisions = FormulaIngredientImporter(
+            {instance.name: instance for instance in self.instances}
+        ).do_import(lines_copy, filename=filename)
+        self.instances.extend(ingr)
+        self.collisions.extend(ingr_collisions)
+
+        return self.instances, self.collisions
 
 
 class ProductLineImporter(Importer):
