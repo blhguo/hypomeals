d4b15206884b54ee9bba51f4ee5078e110f02c99
Brandon Guo <blhguo@gmail.com>
2019-04-10 17:24:30 -0400

    Fix lint

--- hypomeals/meals/forms.py
+++ hypomeals/meals/forms.py
@@ -1185,8 +1185,6 @@ class GoalFilterForm(forms.Form, utils.BootstrapFormControlMixin):
         query = Q()
         if self.cleaned_data["name"]:
             query &= Q(name__icontains=self.cleaned_data["name"])
-        if not user.is_admin:
-            pass
         else:
             if self.cleaned_data["users"]:
                 query &= Q(user__in=self.cleaned_data["users"])
