819603251fe97dcab12060aedaaffaedbe05e602
Xingyu Chen <xc77@duke.edu>
2019-04-14 21:51:46 -0400

    First Draft

--- hypomeals/meals/scheduling.py
+++ hypomeals/meals/scheduling.py
@@ -6,12 +6,15 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import List, Mapping, Set
 
+from meals.models import GoalItem, ManufacturingLine, GoalSchedule
+from meals.utils import compute_end_time
+
 logger = logging.getLogger(__name__)
 
 
 @dataclass
 class Item:
-    id: int
+    item: GoalItem
     hours: int
     groups: Set[str]
 
@@ -57,13 +60,56 @@ def schedule(
         start.isoformat(),
         end.isoformat(),
     )
+    # Prioritize item with earliest deadline, with a tiebreaker of shortest
+    # time first
+    items.sort(key=lambda item: (item.item.completion_time, item.hours))
+    ml_schedules = dict()
+    for ml in ManufacturingLine.objects.all():
+        relevant_goals = GoalSchedule.objects.filter(line = ml,
+                                                     start_time__lte=end,
+                                                     end_time__gte=start).order_by("start_time")
+        time_block = []
+        cur_start = start
+        # Special check
+        first_goal_item = relevant_goals[0]
+        if first_goal_item.start_time < start:
+            cur_start = first_goal_item.start_time
+        for schedule in relevant_goals:
+            goal_item = schedule.goal_item
+            time_block.append((cur_start, goal_item.start))
+            cur_start = goal_item.end
+        if (cur_start < end):
+            time_block.append((cur_start, end))
+        ml_schedules[ml.shortname] = time_block
+
     for item in items:
-        schedules.append(
-            Schedule(
-                item.id,
-                start,
-                start + timedelta(hours=item.hours),
-                next(iter(item.groups)),
+        min_earliest_time = end
+        min_ml = None
+        for ml in item.groups:
+            earliest_time = end
+            for id, (st, et) in enumerate(ml_schedules[ml]):
+                if compute_end_time(st, item.hours) <= et:
+                    earliest_time = st
+                    earliest_block_id = id
+            if earliest_time <= min_earliest_time:
+                min_earliest_time = earliest_time
+                min_ml = ml
+                min_block_id = earliest_block_id
+        if min_ml is None:
+            raise ScheduleException("Unable to schedule: too many items.")
+        else:
+            item_end_time = compute_end_time(min_earliest_time, item.hours)
+            schedules.append(
+                Schedule(
+                    item.id,
+                    min_earliest_time,
+                    item_end_time,
+                    min_ml,
+                )
             )
-        )
+            st, et = ml_schedules[ml][min_block_id]
+            if (et == item_end_time):
+                del ml_schedules[ml][min_block_id]
+            else:
+                ml_schedules[ml][min_block_id] = (item_end_time, et)
     return list(map(Schedule.to_dict, schedules))
